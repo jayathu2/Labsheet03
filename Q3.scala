7becf158ab92cd01820db8158b9f5c8cb4058960
jayathu sanka <shjayathusanka@gmail.com>
2024-07-01 16:29:56 +0530

    comment

--- Q3.scala
+++ Q3.scala
@@ -11,5 +11,4 @@ object ArithmeticMeanCalculator{
     val num2=10
     val result=meanRounded(num1,num2)
     println(s"The arithmetic mean of $num1 and $num2, rounded to two decimal places, is $result")
-  }
 }
---
+++ Q4.scala
@@ -0,0 +1,16 @@
+object SumEvenNumbers{
+    def main(args:Array[String]):Unit={
+        val numbers=List(1,2,3,4,5,6)
+        println(s"Sum of even numbers:${SumEvenNumbers(numbers)}")
+
+
+    }
+
+    def SumEvenNumbers(numbers: List[Int]): Int={
+        numbers.filter(_%2==0).sum
+
+    }
+
+
+
+}
